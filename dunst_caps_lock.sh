f6599addb264574b77138d24160cdb4180112455
Brandon <brandonlanuevo@gmail.com>
2020-03-02 16:31:14 -0800

    ~

---
+++ dunst_caps_lock.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#lockkey.sh
+
+sleep 0.2
+
+value="$(xset q | grep -i caps | cut -c 22-24)"
+
+if [ $value == "on" ];
+    then
+        output="caps lock is on"
+    else
+        output="caps lock is off"
+fi
+
+notify-send -t 600 "$output"
